bd8e065d10c30e3719f94b19b9d282db7eaa1f69
suchenlu <suchenlu0112@163.com>
2016-08-05 18:07:24 +0800

    version9

--- main/js/base_same.js
+++ main/js/base_same.js
@@ -1,3 +1,59 @@
+function placeholderSupport() {
+
+        // 判断浏览器是否支持placeholder属性
+        supportPlaceholder = 'placeholder' in document.createElement('input'),
+
+            placeholder = function(input) {
+
+                var text = input.attr('placeholder'),
+                    defaultValue = input.defaultValue;
+
+                if (!defaultValue) {
+
+                    input.val(text).addClass("phcolor");
+                }
+
+                input.focus(function() {
+
+                    if (input.val() == text) {
+
+                        $(this).val("");
+                    }
+                });
+
+
+                input.blur(function() {
+
+                    if (input.val() == "") {
+
+                        $(this).val(text).addClass("phcolor");
+                    }
+                });
+
+                //输入的字符不为灰色
+                input.keydown(function() {
+
+                    $(this).removeClass("phcolor");
+                });
+            };
+
+        // 当浏览器不支持placeholder属性时，调用placeholder函数
+        if (!supportPlaceholder) {
+
+            $('input').each(function() {
+
+                text = $(this).attr("placeholder");
+
+                if ($(this).attr("type") == "text") {
+
+                    placeholder($(this));
+                }
+            });
+        }
+
+    };
+    placeholderSupport();
+
 //头部登陆后状态
 $(".nav_right .After_landing a").click(function() {
 	$(".nav_right .After_landing").toggleClass("hei_class")
@@ -105,58 +161,3 @@ function hHb() {
 		$(this).hide()
 	})
 }
-function placeholder() {
-
-        // 判断浏览器是否支持placeholder属性
-        supportPlaceholder = 'placeholder' in document.createElement('input'),
-
-            placeholder = function(input) {
-
-                var text = input.attr('placeholder'),
-                    defaultValue = input.defaultValue;
-
-                if (!defaultValue) {
-
-                    input.val(text).addClass("phcolor");
-                }
-
-                input.focus(function() {
-
-                    if (input.val() == text) {
-
-                        $(this).val("");
-                    }
-                });
-
-
-                input.blur(function() {
-
-                    if (input.val() == "") {
-
-                        $(this).val(text).addClass("phcolor");
-                    }
-                });
-
-                //输入的字符不为灰色
-                input.keydown(function() {
-
-                    $(this).removeClass("phcolor");
-                });
-            };
-
-        // 当浏览器不支持placeholder属性时，调用placeholder函数
-        if (!supportPlaceholder) {
-
-            $('input').each(function() {
-
-                text = $(this).attr("placeholder");
-
-                if ($(this).attr("type") == "text") {
-
-                    placeholder($(this));
-                }
-            });
-        }
-
-    };
-    placeholder();
